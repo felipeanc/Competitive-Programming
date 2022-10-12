ee7729bf584fbd04e9c2cae2bdef3f5536dcf74b
Felipe <felipenunesaugusto@gmail.com>
2022-10-11 21:09:04 -0300

    Add question i

---
+++ contests/2022-2023 ICPC Brazil Subregional Programming Contest/i.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  for(int i = 0; i < 8; ++i){
+    int n; cin >> n; 
+    if(n == 9){cout << "F" << endl; return;}
+  }
+  cout << "S" << endl;
+  return 0;
+}
---
+++ contests/2022-2023 ICPC Brazil Subregional Programming Contest/i.py
@@ -0,0 +1,2 @@
+s = input()
+print("F" if '9' in s else "S")
